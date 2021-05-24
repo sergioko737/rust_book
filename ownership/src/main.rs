31abe54d907cb4dab035839c0754ed60f0ba1398
sergioko737 <s29585476@gmail.com>
2021-05-25 01:13:07 +0300

    ADD some examples of references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -7,6 +7,18 @@ fn main() {
     some_print(s, x);
     println!("and again {}", x);
 
+    let mut sx = String::from("privet");
+
+    let r1 = &sx; // no problem
+    let r2 = &sx; // no problem
+    println!("{} and {}", r1, r2);
+    // r1 and r2 are no longer used after this point
+    println!("{} and {}", r1, r2);
+    let r3 = &mut sx; // no problem
+    println!("{}", r3);
+    // next line if uncommented will cause a compilation problem
+    // because mutable borrow already happened at line 18
+    // println!("{} and {}", r1, r2);
 
 }
 
