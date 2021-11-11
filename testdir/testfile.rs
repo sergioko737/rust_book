c2e2a323d2ddf162990c77556782558e512209c9
sergioko737 <s29585476@gmail.com>
2021-11-11 15:03:03 +0200

    no lines with lock touched

--- testdir/testfile.rs
+++ testdir/testfile.rs
@@ -1,4 +1,4 @@
-NON_BREAKING CHANGE substrate/frame/bags-list/src/lib.rs:			concat!("[{:?}] 👜", $patter), <frame_system::Pallet<T>>::block_number() $(, $values)*
+substrate/frame/bags-list/src/lib.rs:			concat!("[{:?}] 👜", $patter), <frame_system::Pallet<T>>::block_number() $(, $values)*
 some more substrate/client/finality-grandpa/src/environment.rs:					"👴 Applying GRANDPA set change to new set with {} authorities",
 substrate/client/finality-grandpa/src/environment.rs:				afg_log!(initial_sync, "👴 Applying GRANDPA set change to new set {:?}", set_ref);
 substrate-matrix-faucet/src/bot/index.ts:    logger.warn(`🏴‍☠️ Ignored request from an ignored account: ${sender}`);
