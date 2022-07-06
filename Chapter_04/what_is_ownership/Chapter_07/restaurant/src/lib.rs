d26f99fa4dae53df722f16e6a5e18d2e54cca760
sergioko737 <s29585476@gmail.com>
2022-07-06 19:33:57 +0300

    ch07 add front of house mod

--- Chapter_04/what_is_ownership/Chapter_07/restaurant/src/lib.rs
+++ Chapter_04/what_is_ownership/Chapter_07/restaurant/src/lib.rs
@@ -1,8 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
     }
 }
